63a741b96305668c0c0cfa344697df822be92478
Deniz Frick <deniz.frick@student.tuwien.ac.at>
2026-05-24 12:41:22 +0200

    Added more scaffold for the two formulations

--- src/Model.jl
+++ src/Model.jl
@@ -8,24 +8,41 @@ function create_model(graph::SimpleWeightedGraph, formulation)
     )
 
     model = Model(() -> Gurobi.Optimizer(env))
-    set_attribute(model, MOI.LazyConstraintCallback(), cb_data -> lazy_constraint_callback(cb_data, graph))
 
-
-    @show graph
     # nv = number of vertices
     n = nv(graph)
-    @show n
 
-    # selects traveled edges
+    # General Constraints
     @variable(model, x[1:n, 1:n], Bin)
     @objective(model, Min, sum(x[i, j] for i in 1:n for j in 1:n if i != j))
+
+    if formulation == "cec"
+        set_attribute(model, MOI.LazyConstraintCallback(), cb_data -> cec_callback(cb_data, graph))
+
+        # Cycle Elimination Constraints
+
+
+
+    elseif formulation == "dcc"
+        set_attribute(model, MOI.LazyConstraintCallback(), cb_data -> dcc_callback(cb_data, graph))
+
+        # Directed Cutset Constraints
+
+
+    else
+        error("Invalid formulation")
+    end
 
     return model
 end
 
 
-function lazy_constraint_callback(cb_data::Gurobi.CallbackData, graph::SimpleWeightedGraph)
+function cec_callback(cb_data::Gurobi.CallbackData, graph::SimpleWeightedGraph)
     @show typeof(cb_data)
     @show graph
+end
 
+function dcc_callback(cb_data::Gurobi.CallbackData, graph::SimpleWeightedGraph)
+    @show typeof(cb_data)
+    @show graph
 end
